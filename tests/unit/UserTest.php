c3ce669bcb936e060df14af28c4268bdc53e401b
MoEmam203 <mostafa.emamdar4@gmail.com>
2022-07-26 20:13:19 +0200

    using setUp method to refactor tests

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -5,69 +5,66 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase {
 
+    protected $user;
+
+    protected function setUp() : void
+    {
+        $this->user = new User();
+    }
+
     /** @test */
     public function that_we_can_get_first_name()
     {
-        $user = new User;
+        $this->user->setFirstName('Mustafa');
 
-        $user->setFirstName('Mustafa');
-
-        $this->assertEquals($user->getFirstName(),'Mustafa');
+        $this->assertEquals($this->user->getFirstName(),'Mustafa');
     }
 
     /** @test */
     public function that_we_can_get_last_name()
     {
-        $user = new User;
+        $this->user->setLastName('Emam');
 
-        $user->setLastName('Emam');
-
-        $this->assertEquals($user->getLastName(),'Emam');
+        $this->assertEquals($this->user->getLastName(),'Emam');
     }
 
     /** @test */
     public function that_we_can_get_full_name()
     {
-        $user = new User;
+        $this->user->setFirstName('Mustafa');
+        $this->user->setLastName('Emam');
 
-        $user->setFirstName('Mustafa');
-        $user->setLastName('Emam');
-
-        $this->assertEquals($user->getFullName(),'Mustafa Emam');
+        $this->assertEquals($this->user->getFullName(),'Mustafa Emam');
     }
 
     /** @test */
     public function that_first_name_and_last_name_are_trimmed()
     {
-        $user = new User;
+        $this->user->setFirstName("Mustafa              ");
+        $this->user->setLastName("       Emam   ");
 
-        $user->setFirstName("Mustafa              ");
-        $user->setLastName("       Emam   ");
-
-        $this->assertEquals($user->getFirstName(),'Mustafa');
-        $this->assertEquals($user->getLastName(),'Emam');
+        $this->assertEquals($this->user->getFirstName(),'Mustafa');
+        $this->assertEquals($this->user->getLastName(),'Emam');
     }
 
     /** @test */
-    public function that_we_can_get_email(){
-        $user = new User();
+    public function that_we_can_get_email()
+    {
+        $this->user->setEmail('mustafa@test.com');
 
-        $user->setEmail('mustafa@test.com');
-
-        $this->assertEquals($user->getEmail(),'mustafa@test.com');
+        $this->assertEquals($this->user->getEmail(),'mustafa@test.com');
     }
 
     /** @test */
     public function that_email_variables_contains_correct_values()
     {
         // set first name , last name , email 
-        $user = new User();
-        $user->setFirstName('Mustafa');
-        $user->setLastName('Emam');
-        $user->setEmail('mustafa@test.com');
+        $this->user->setFirstName('Mustafa');
+        $this->user->setLastName('Emam');
+        $this->user->setEmail('mustafa@test.com');
 
         // get email variables
-        $emailVariables = $user->getEmailVariables();
+        $emailVariables = $this->user->getEmailVariables();
 
         // check that email variables contains full_name & email keys
         $this->assertArrayHasKey('full_name',$emailVariables);
